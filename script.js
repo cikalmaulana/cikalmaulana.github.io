22f2f0ac0297128d81213d83a26a9aba742beb8b
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2021-07-22 10:28:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+    document.getElementById("sideNavMobile").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNavMobile").style.width = "0";
+}
